7f2e5fe4e2d802effb1e42507fd878781bddc260
Ân Nguyễn <thienannguyen10102001@gmail.com>
2024-03-16 16:47:14 +1100

    form and fix mongoose

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,6 +1,6 @@
 import mongoose, { Mongoose } from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URL = process.env.MONGODB_URL;
 
 interface MongooseConnection {
     conn: Mongoose | null;
@@ -10,27 +10,25 @@ interface MongooseConnection {
 let cached: MongooseConnection = (global as any).mongoose;
 
 if (!cached) {
-    cached = (global as any).mongoose = { conn: null, promise: null };
+    cached = (global as any).mongoose = {
+        conn: null,
+        promise: null,
+    };
 }
 
 export const connectToDatabase = async () => {
-    if (cached.conn) {
-        return cached.conn;
-    }
+    if (cached.conn) return cached.conn;
 
-    if (!MONGODB_URI) {
-        throw new Error(
-            "Please define the MONGODB_URI environment variable inside .env.local"
-        );
-    }
+    if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
 
     cached.promise =
         cached.promise ||
-        mongoose.connect(MONGODB_URI, {
-            dbName: "vivifyr",
+        mongoose.connect(MONGODB_URL, {
+            dbName: "imaginify",
             bufferCommands: false,
         });
 
     cached.conn = await cached.promise;
+
     return cached.conn;
 };
